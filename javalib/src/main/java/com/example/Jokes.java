f83bff2c9625d417b29a6095367ca44cad426578
BrenoMarques <breno@ccrm.com.br>
2016-12-28 18:09:44 -0300

    Etapa 1: Criar uma biblioteca Java Criar uma biblioteca Java que fornece as piadas. Crie um novo projeto Java do Gradle usando o assistente do Android Studio ou trabalhando por sua conta. Depois, Introduza uma dependência de projeto entre seu aplicativo e a nova biblioteca Java.

---
+++ javalib/src/main/java/com/example/Jokes.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Jokes {
+
+    public static String get(){
+
+        return "O sujeito vai pedir aumento pro chefe:\n" +
+                "Acho melhor o senhor me promover! Tem muitas empresas me procurando...\n" +
+                "É mesmo? - pergunta o chefe, irônico - Quais são essas empresas?\n" +
+                "A empresa de eletricidade, a empresa de saneamento, a empresa de telefone e as maiores empresas de cobrança do país!";
+    }
+
+
+
+}
